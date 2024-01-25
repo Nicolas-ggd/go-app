ed1b41318994d1fafe846dd1e2cbb7fc2d34fd96
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2024-01-26 02:03:41 +0400

    create middleware to check receiver in database

--- cmd/api/chat.go
+++ cmd/api/chat.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"websocket/internal/models"
@@ -12,6 +13,13 @@ func (h *Handler) InsertMessageHandler(c *gin.Context) {
 	err := c.ShouldBind(&chat)
 	if err != nil {
 		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "Invalid request data, missing request fields"})
+		return
+	}
+
+	isExist, err := h.UserService.CheckUserIsExist(chat.ReceiverID)
+	fmt.Println(chat.ReceiverID, isExist)
+	if err != nil || !isExist {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User doesn't exist"})
 		return
 	}
 
