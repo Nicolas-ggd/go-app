13c25c589c3912835e1f8b66a5545f6d653afbf5
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2024-03-20 19:37:43 +0400

    update: use gin builtin middleware

--- pkg/http/routes/main.go
+++ pkg/http/routes/main.go
@@ -14,7 +14,11 @@ func ServeApp() {
 	router := gin.Default()
 	gin.SetMode(gin.TestMode)
 
-	router.Use(middleware.CORSOptions())
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(middleware.CORSOptions(), gin.Logger(), gin.Recovery())
 
 	h := http.Handler{
 		Repository: models.Repository{
