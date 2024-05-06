2fab4ea3d21a966fb6099b59747803d55ae9fc1e
YAMATO <Nicolas-ggd>
2024-05-06 18:34:00 +0400

    [REPLACE] replace gin with build-in net/http

--- pkg/http/routes/main.go
+++ pkg/http/routes/main.go
@@ -1,41 +1,17 @@
 package routes
 
-import (
-	"github.com/gin-gonic/gin"
-	"log"
-	"websocket/internal/db"
-	"websocket/internal/models"
-	"websocket/pkg/http/middleware"
-	http "websocket/pkg/http/rest"
-	"websocket/pkg/http/ws"
-)
+import "net/http"
 
 func ServeApp() {
-	router := gin.Default()
-	gin.SetMode(gin.TestMode)
+	router := http.NewServeMux()
 
-	// Global middleware
-	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
-	// By default gin.DefaultWriter = os.Stdout
-	// Recovery middleware recovers from any panics and writes a 500 if there was one.
-	router.Use(middleware.CORSOptions(), gin.Logger(), gin.Recovery())
-
-	h := http.Handler{
-		Repository: models.Repository{
-			DB: db.DB,
-		},
-		Websocket: ws.NewWebsocket(),
+	server := http.Server{
+		Addr:    "8000",
+		Handler: router,
 	}
 
-	api := router.Group("api")
-	{
-		AuthRoutes(api.Group("account"), &h)
-	}
-
-	api.GET("/ws", h.ServeWs())
-
-	err := router.Run(":7000")
+	err := server.ListenAndServe()
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 }
